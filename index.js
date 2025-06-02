916b5d02575a54cec0be5b1dea7b7be784b80d6a
Neymo5058 <vadam7480@gmail.com>
2025-06-02 11:24:56 -0400

    initialiaze

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const uri =
+  'mongodb+srv://username123:sami5058@cluster0.dy6dz3y.mongodb.net/TP3_Jonathan_Sami-main?retryWrites=true&w=majority&appName=Cluster0';
+
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+};
+
+mongoose.connect(uri, clientOptions).then(() => console.log('Connected to DB'));
+
+// Middleware
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    const errors = Object.values(err.errors).map((e) => e.message);
+    return res.status(400).json({
+      status: 'fail',
+      errors,
+    });
+  }
+
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message || 'Erreur interne du serveur',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});

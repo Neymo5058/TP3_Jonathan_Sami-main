f553a81208af65864c60b4e532408eddf63b0e07
anjo7778 <jonatantd2@gmail.com>
2025-06-11 12:36:19 -0400

    Update grimoiresRouter.js

--- router/grimoiresRouter.js
+++ router/grimoiresRouter.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import GrimoireController from '../controller/grimoire-controller.js';
-import { authenticate, authorizeAdminOrOwner } from '../middleware/auth.js';
-import GrimoireModel from '../model/GrimoireModel.js';
+import { authenticate } from '../middleware/auth.js';
+
 
 const router = express.Router();
-const getGrimoireOwner = async (req) => {
-  const grimoire = await GrimoireModel.findById(req.body.grimoireId);
-  return grimoire?.owner;
-};
+
 
 router.post('/create', authenticate, GrimoireController.create);
 router.post('/add-spell', authenticate, GrimoireController.addSpell);
